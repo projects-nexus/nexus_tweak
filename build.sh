b9ae3f3af9a223f55dd5673ed17daa6ae4a5c60f
Ayush <90475639+ImSpiDy@users.noreply.github.com>
2022-02-08 02:59:48 +0530

    Import: workflow & Build Script
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+ZIPNAME=Nexus-Tweak-X1.zip
+
+function push() {
+curl -F document=@$1 "https://api.telegram.org/bot${token}/sendDocument" \
+     -F chat_id="${chat_id}"  \
+     -F "disable_web_page_preview=true" \
+     -F "parse_mode=html" \
+     -F caption="`cat changelog.txt`"
+}
+
+zip -r9 "${ZIPNAME}" *
+
+push "${ZIPNAME}"
