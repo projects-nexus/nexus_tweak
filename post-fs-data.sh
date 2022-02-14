4ec9c8848e459797b43d77906fd0e8a5c8ed4b4d
LOOPER <iamloooper@gmail.com>
2022-02-14 14:24:40 +0500

    Enable/Disable EAS accord. to profiles
    Docs: https://www.kernel.org/doc/html/v5.9/admin-guide/sysctl/kernel.html#sched-energy-aware and https://source.android.com/devices/tech/perf/boot-times
    Signed-off-by: LOOPER <iamloooper@gmail.com>

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,2 +1,11 @@
 #!/system/bin/sh
-#wip
+# NeXus Tweaks
+
+# Improve boot time by tuning I/O scheduler parameters (Cherrypicked from https://source.android.com/devices/tech/perf/boot-times)
+ write "/sys/block/sda/queue/iostats" "0"
+ write "/sys/block/sda/queue/scheduler" "cfq"
+ write "/sys/block/sda/queue/iosched/slice_idle" "0"
+ write "/sys/block/sda/queue/read_ahead_kb" "2048"
+ write "/sys/block/sda/queue/nr_requests" "256"
+ write "/sys/block/dm-0/queue/read_ahead_kb" "2048"
+ write "/sys/block/dm-1/queue/read_ahead_kb" "2048"
