7b3e7c570127df2aafada959d5b561018131cc19
Vishal Singh Rajput <vishal.rockstar7011@gmail.com>
2022-02-17 08:38:50 +0530

    post-fs-data: add conflict module remover script
    not tested

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,2 +1,9 @@
 #!/system/bin/sh
-#wip
+module=/data/adb/modules/
+conflict=$(find /data/adb -type d -not -path "*modules/nexus*" -iname "*module.prop*")
+for nex in $conflict; do
+   search=$(echo "$nex" | sed -e 's/\// /g' | awk '{print $4}')
+   if grep -q 'tweak' $nex 2>/dev/null; then
+     touch $module$search/remove
+   fi
+done
