295ae887e01536c3dfcc9e5e960edabd71fb80f4
ImSpiDy <SpiDy2713@gmail.com>
2022-03-06 06:41:23 +0000

    service.sh: add art optimizer
    * This will optimise ART runtime, reduce idle drain and fixes lags ( after restoring apps with Migrate or dirty flashing rom )
    
    Signed-off-by: ImSpiDy <SpiDy2713@gmail.com>

--- service.sh
+++ service.sh
@@ -1,6 +1,8 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
+sprop=/data/adb/modules/nexus/system.prop
+
 # Detect whether Unlocked into System
 while $(dumpsys window policy | grep mIsShowing | awk -F= '{print $2}')
 do
@@ -9,6 +11,18 @@ done
 
 # sleep extra 10s.
 sleep 10
+
+# excute only once after flashing module
+boot=$(getprop nex.boot)
+
+if [ "$boot" == "0" ]; then
+
+# ART optimizer
+su -c cmd package bg-dexopt-job
+
+echo nex.boot=1 >> $sprop
+
+fi
 
 su -lp 2000 -c "cmd notification post -S bigtext -t Nexus-Tweaks tag Activated"
 
