089d0b75fb6f2574765638cc78e00e98fc95d884
akirasupr <vishal.rockstar7011@gmail.com>
2022-03-07 02:24:54 +0530

    post-fs-data: add blocker
    detect conflict module and remove
    
    Signed-off-by: akirasupr <vishal.rockstar7011@gmail.com>

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,9 +1,6 @@
 #!/system/bin/sh
-module=/data/adb/modules/
-conflict=$(find /data/adb -type d -not -path "*modules/nexus*" -iname "*module.prop*")
-for nex in $conflict; do
-   search=$(echo "$nex" | sed -e 's/\// /g' | awk '{print $4}')
-   if grep -q 'tweak' $nex 2>/dev/null; then
-     touch $module$search/remove
-   fi
-done
+MODDIR=${0%/*}
+
+$MODDIR/system/bin/blocker
+
+exit 0
