3491521263d7a06190b84084631a4b82b1acdb6b
imspidy <spidy2713@gmail.com>
2022-03-26 02:52:30 +0530

    layout: add Boundary for button [ Test ]
    Signed-off-by: imspidy <spidy2713@gmail.com>

--- app/src/main/java/org/frap129/spectrum/MainActivity.java
+++ app/src/main/java/org/frap129/spectrum/MainActivity.java
@@ -19,6 +19,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.net.Uri;
 
