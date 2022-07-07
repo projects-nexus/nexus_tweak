20d598cc86fb47c1159744f44b97fe47bdb6207f
wHo-EM-i <ehteshammalik18998@gmail.com>
2022-07-07 19:24:31 +0530

    nexus: switch to NexusToast app
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -17,6 +17,6 @@ mv -f nexus/product system
 mv -f nexus/* system/bin
 rm -rf nexus
 
-zip -r9 "${ZIPNAME}" * -x build.sh -x *.github* -x *BeastMode*
+zip -r9 "${ZIPNAME}" * -x build.sh -x *.github*
 
 push "${ZIPNAME}"
