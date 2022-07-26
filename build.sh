2bd0be6fa005d1fcd3c70b5e069db516649646f2
ImSpiDy <spidy2713@gmail.com>
2022-07-26 00:57:50 +0000

    build: add nexus tweak app
    Signed-off-by: ImSpiDy <spidy2713@gmail.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 ZIPNAME=Nexus-Tweak-x0.${buildNo}.zip
+APP_DIR=nexus/product/app/NexusTweak
 
 function push() {
 curl -F document=@$1 "https://api.telegram.org/bot${token}/sendDocument" \
@@ -9,6 +10,17 @@ curl -F document=@$1 "https://api.telegram.org/bot${token}/sendDocument" \
      -F "parse_mode=html" \
      -F caption="`cat changelog.md`"
 }
+
+# download nexus tweak app
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+apt update && apt install gh
+echo $gh_token > mytoken.txt
+gh auth login --with-token < mytoken.txt
+rm -rf mytoken.txt
+mkdir -p $APP_DIR
+gh release download -p '*.apk' --repo https://github.com/ImSpiDy/NexusTweak-App --dir $APP_DIR
 
 mkdir -p system/bin
 mkdir -p nex
